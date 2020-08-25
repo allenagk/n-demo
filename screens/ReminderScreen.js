c92e2fe52a12e4b67d0347b069ad1c132df83cc7
Allen <allenagk@gmail.com>
2020-08-25 23:57:31 +0900

    Adding views from the dialog prompt

--- screens/ReminderScreen.js
+++ screens/ReminderScreen.js
@@ -1,28 +1,106 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Button,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  FlatList,
+  Alert,
+} from "react-native";
 import colors from "../constants/colors";
 
 import ProgressBar from "../components/ProgressBar";
-import { ScrollView } from "react-native-gesture-handler";
+//import { ScrollView } from "react-native-gesture-handler";
 
 const ReminderScreen = (props) => {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = (x) => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: x },
+    ]);
+  };
+
+  const onButtonPress = () => {
+    Alert.prompt(
+      "Title of the window",
+      "Description of the title",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel",
+        },
+        {
+          text: "OK",
+          onPress: (x) => {
+            //goalInputHandler(x);
+            addGoalHandler(x);
+            //goalInputHandler(x);
+            //console.log(x);
+          },
+        },
+      ],
+      "plain-text"
+    );
+  };
+
   return (
     <View style={styles.screen}>
-      {/* <ScrollView> */}
-      {/* Uncomment scrollview if want to scroll the page */}
-      <ProgressBar title={"目的 A"} completion={40} />
-      <ProgressBar title={"目的 B"} completion={30} />
-      <ProgressBar title={"目的 C"} completion={30} />
-      {/* </ScrollView> */}
+      <View style={styles.inputContainer}>
+        {/* <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        /> */}
+        <Button title="ADD" onPress={onButtonPress} />
+      </View>
+
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <View style={styles.listItem}>
+            <Button title={itemData.item.value}></Button>
+            {/* <Text>{itemData.item.value}</Text> */}
+          </View>
+        )}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   screen: {
-    flex: 1,
-    backgroundColor: colors.backgrnd,
-    padding: 15,
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: "#ccc",
+    borderColor: "black",
+    borderWidth: 1,
   },
 });
 
